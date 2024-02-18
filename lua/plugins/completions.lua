9dd85ab5aff54c2c672a322ae9e947070752b728
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-02-18 11:41:44 -0800

    feat: add completions

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,82 @@
+return {
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "onsails/lspkind.nvim",
+        },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                completion = {
+                    keyword_length = 3,
+                },
+                experimental = {
+                    ghost_text = true,
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                formatting = {
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol",
+                        maxwidth = function()
+                            return math.floor(0.4 * vim.o.columns)
+                        end,
+                        ellipsis_char = "...",
+                        show_labelDetails = true,
+                        menu = {
+                            luasnip = " [LuaSnip]",
+                            nvim_lsp = " [LSP]",
+                            buffer = " [Buffer]",
+                            path = " [Path]",
+                        },
+                    }),
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-y>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end),
+                }),
+            })
+        end,
+    },
+}

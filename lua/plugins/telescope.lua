79fef9e8b057437d33f6bfa70f8ec58412c91ea2
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-06-07 23:31:38 +0800

    feat: add current buffer fuzzy find

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {})
             vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
             vim.keymap.set("n", "gr", builtin.lsp_references, {})
             vim.keymap.set("n", "gi", builtin.lsp_implementations, {})

e8e8fd43a9f062e54b01c6f3719c1fbc1ba404a4
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-06-15 23:01:31 +0800

    feat: remove none_ls formatter for python and lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,9 +4,6 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.prettier,
             },
         })

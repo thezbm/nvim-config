76aebad2d9c77b4c02804a3098b2de445c4485cd
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-06-15 18:10:28 +0800

    feat: add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+local function find_shell()
+	local shells = { "pwsh", "powershell", "zsh", "bash" }
+	for _, shell in ipairs(shells) do
+		if vim.fn.executable(shell) == 1 then
+			return shell
+		end
+	end
+end
+
+return {
+	"akinsho/toggleterm.nvim",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = "<C-\\>",
+			direction = "float",
+			close_on_exit = true,
+			float_opts = {
+				border = "curved",
+				winblend = 10,
+			},
+			shell = find_shell(),
+		})
+	end,
+}

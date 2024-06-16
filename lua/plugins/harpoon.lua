cefabe744615a4686534ce7e077f6bc93ec289fd
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-06-16 10:50:16 +0800

    chore: make harpoon and toggleterm lazy

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,6 +2,7 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    event = "VeryLazy",
     config = function()
         local harpoon = require("harpoon")
         harpoon:setup()

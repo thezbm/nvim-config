36615bdc1afdfa83134c0ed582aa131422a6d590
Beiming Zhang <thebeimingzhang@outlook.com>
2024-10-01 20:22:09 +0800

    feat: add git hunk key bindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,10 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    opts = {}
+    config = function()
+        require("gitsigns").setup({})
+        vim.keymap.set("n", "<C-p>", ":Gitsigns prev_hunk<cr>")
+        vim.keymap.set("n", "<C-n>", ":Gitsigns next_hunk<cr>")
+        vim.keymap.set("n", "<leader>p", ":Gitsigns preview_hunk<cr>")
+    end
 }
-
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,5 @@ return {
         harpoon:setup()
         vim.keymap.set("n", "<C-h>", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     end
 }

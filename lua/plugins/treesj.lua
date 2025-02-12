7ac9af27bb9bd23a7cc0d4f137fd08601acd47e7
Beiming Zhang <thebeimingzhang@outlook.com>
2025-02-12 23:17:21 +0800

    fix: increase treesj max length

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,7 +3,10 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     keys = { "<space>m" },
     config = function()
-        require("treesj").setup({ use_default_keymaps = false })
+        require("treesj").setup({
+            use_default_keymaps = false,
+            max_join_length = 200,
+        })
         vim.keymap.set("n", "<leader>m", require("treesj").toggle)
     end
 }
